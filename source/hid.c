927c885e87f797769c8a588bce6e70cc7849502a
d0k3 <tore.anon@gmail.com>
2016-04-05 23:19:29 +0200

    Improve button react times

--- source/hid.c
+++ source/hid.c
@@ -4,7 +4,16 @@ u32 InputWait() {
     u32 pad_state_old = HID_STATE;
     while (true) {
         u32 pad_state = HID_STATE;
-        if (pad_state ^ pad_state_old)
+        if (!(~pad_state & BUTTON_ANY)) { // no buttons pressed
+            pad_state_old = pad_state;
+            continue;
+        }
+        if (pad_state == pad_state_old)
+            continue;
+        //Make sure the key is pressed
+        u32 t_pressed = 0;
+        for(; (t_pressed < 0x13000) && (pad_state == HID_STATE); t_pressed++);
+        if (t_pressed >= 0x13000)
             return ~pad_state;
     }
 }
