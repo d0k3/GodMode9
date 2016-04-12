1750428342da184d5827f0025b2fbe3feb8a7921
d0k3 <tore.anon@gmail.com>
2016-04-12 12:35:59 +0200

    Replace tabs with spaces

--- source/abstraction/gw-start.s
+++ source/abstraction/gw-start.s
@@ -6,36 +6,36 @@
 .arm
 
 _vectors:
-	ldr pc, =InfiniteLoop
-	.pool
-	ldr pc, =InfiniteLoop
-	.pool
-	ldr pc, =InfiniteLoop
-	.pool
-	ldr pc, =InfiniteLoop
-	.pool
-	ldr pc, =InfiniteLoop
-	.pool
-	ldr pc, =InfiniteLoop
-	.pool
+    ldr pc, =InfiniteLoop
+    .pool
+    ldr pc, =InfiniteLoop
+    .pool
+    ldr pc, =InfiniteLoop
+    .pool
+    ldr pc, =InfiniteLoop
+    .pool
+    ldr pc, =InfiniteLoop
+    .pool
+    ldr pc, =InfiniteLoop
+    .pool
 
 _start:
-	ldr sp,=0x22140000
+    ldr sp,=0x22140000
 
-	@@wait for the arm11 kernel threads to be ready
-	ldr r1, =0x10000
-	waitLoop9:
-		sub r1, #1
+    @@wait for the arm11 kernel threads to be ready
+    ldr r1, =0x10000
+    waitLoop9:
+        sub r1, #1
 
-		cmp r1, #0
-		bgt waitLoop9
+        cmp r1, #0
+        bgt waitLoop9
 
-	ldr r1, =0x10000
-	waitLoop92:
-		sub r1, #1
+    ldr r1, =0x10000
+    waitLoop92:
+        sub r1, #1
 
-		cmp r1, #0
-		bgt waitLoop92
+        cmp r1, #0
+        bgt waitLoop92
 
     @ Enable caches
     mrc p15, 0, r4, c1, c0, 0  @ read control register
@@ -45,12 +45,12 @@ _start:
     orr r4, r4, #(1<<0)        @ - mpu enable
     mcr p15, 0, r4, c1, c0, 0  @ write control register
     
-	ldr sp,=0x22160000
-	ldr	r3, =main
-	blx r3
+    ldr sp,=0x22160000
+    ldr    r3, =main
+    blx r3
 .pool
 
 InfiniteLoop:
-	b InfiniteLoop
+    b InfiniteLoop
 
 #endif // EXEC_GATEWAY
