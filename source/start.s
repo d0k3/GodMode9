7ca396ccfb543b14d0ff040e8a6dd80630f156c8
d0k3 <tore.anon@gmail.com>
2017-03-10 12:22:30 +0100

    Improve start.s file (thanks to @TuxSH and @Wintermute)

--- source/start.s
+++ source/start.s
@@ -91,6 +91,12 @@ _skip_gw:
         strlt r2, [r0], #4
         blt .bss_clr
 
+    @ Flush caches
+    mov r5, #0
+    mcr p15, 0, r5, c7, c5, 0  @ flush I-cache
+    mcr p15, 0, r5, c7, c6, 0  @ flush D-cache
+    mcr p15, 0, r5, c7, c10, 4 @ drain write buffer
+
     @ Give read/write access to all the memory regions
     ldr r5, =0x33333333
     mcr p15, 0, r5, c5, c0, 2 @ write data access
@@ -130,12 +136,6 @@ _skip_gw:
     orr r4, r4, #(1<<2)        @ - data cache enable
     orr r4, r4, #(1<<0)        @ - mpu enable
     mcr p15, 0, r4, c1, c0, 0  @ write control register
-
-    @ Flush caches
-    mov r5, #0
-    mcr p15, 0, r5, c7, c5, 0  @ flush I-cache
-    mcr p15, 0, r5, c7, c6, 0  @ flush D-cache
-    mcr p15, 0, r5, c7, c10, 4 @ drain write buffer
 
     @ Fixes mounting of SDMC
     ldr r0, =0x10000020
