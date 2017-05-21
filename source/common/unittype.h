5a5b60c31e91322062961144b63d78e5232424f7
d0k3 <tore.anon@gmail.com>
2017-05-21 13:18:11 +0200

    Fix: mentions of A9LH in user prompts
    Fixes #65

--- source/common/unittype.h
+++ source/common/unittype.h
@@ -10,11 +10,12 @@
 #define IS_DEVKIT   ((*(vu8*) (0x01FFB800+0x19)) != 0x0)
 
 // see: https://3dbrew.org/wiki/CONFIG11_Registers
+// (also returns true for sighaxed systems, maybe change the name later?)
 #define IS_A9LH     ((*(vu32*) 0x101401C0) == 0)
 
 // https://www.3dbrew.org/wiki/CONFIG9_Registers
 // (actually checks for an unlocked OTP)
 #define IS_UNLOCKED (!((*(vu8*)0x10000000) & 0x2))
 
-// A9LH + unlocked = SigHax
+// A9LH + unlocked == SigHax
 #define IS_SIGHAX   (IS_A9LH && IS_UNLOCKED)
