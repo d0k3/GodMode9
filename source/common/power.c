9f135f225aa741600a6be891d2fbb3c3db4c710a
al3x10m <al3x.10m@gmail.com>
2017-08-08 17:47:49 +0300

    Improved brightness table

--- source/common/power.c
+++ source/common/power.c
@@ -4,12 +4,12 @@
 #include "timer.h"
 #include "pxi.h"
 
-static const u8 br_settings[] = {0x1F, 0x3F, 0x7F, 0xBF};
+static const u8 br_settings[] = {0x10, 0x17, 0x1E, 0x25, 0x2C, 0x34, 0x3C, 0x44, 0x4D, 0x56, 0x60, 0x6B, 0x79, 0x8C, 0xA7, 0xD2};
 static int prev_brightness = -1;
 void CheckBrightness() {
     u8 curSlider;
     I2C_readRegBuf(I2C_DEV_MCU, 0x09, &curSlider, 1);
-    curSlider >>= 4;
+    curSlider >>= 2;
     if (curSlider != prev_brightness) {
         PXI_Wait();
         PXI_Send(br_settings[curSlider]);
