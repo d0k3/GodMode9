29b05aee54d3b65dd8ccf7d744f208801be07225
d0k3 <tore.anon@gmail.com>
2018-09-11 00:00:24 +0200

    Less strict checks for AGBSAVE
    This fixes weird issue #412

--- arm9/source/game/gba.c
+++ arm9/source/game/gba.c
@@ -16,10 +16,11 @@ u32 ValidateAgbSaveHeader(AgbSaveHeader* header) {
         return 1;
         
     // reserved area checks
-    for (u32 i = 0; i < sizeof(header->reserved0); i++) if (header->reserved0[i] != 0xFF) return 1;
-    for (u32 i = 0; i < sizeof(header->reserved1); i++) if (header->reserved1[i] != 0xFF) return 1;
-    for (u32 i = 0; i < sizeof(header->reserved2); i++) if (header->reserved2[i] != 0xFF) return 1;
-    for (u32 i = 0; i < sizeof(header->reserved3); i++) if (header->reserved3[i] != 0xFF) return 1;
+    // disabled due to a weird quirk https://github.com/d0k3/GodMode9/issues/412
+    // for (u32 i = 0; i < sizeof(header->reserved0); i++) if (header->reserved0[i] != 0xFF) return 1;
+    // for (u32 i = 0; i < sizeof(header->reserved1); i++) if (header->reserved1[i] != 0xFF) return 1;
+    // for (u32 i = 0; i < sizeof(header->reserved2); i++) if (header->reserved2[i] != 0xFF) return 1;
+    // for (u32 i = 0; i < sizeof(header->reserved3); i++) if (header->reserved3[i] != 0xFF) return 1;
     
     // all fine if arriving here
     return 0;
