414e031d789308f01bb94d8a5b7d22987a69220e
QCFium <rin20042004@gmail.com>
2018-09-13 17:47:48 +0900

    Improved file loading time

--- arm9/source/filesys/fsdir.c
+++ arm9/source/filesys/fsdir.c
@@ -5,26 +5,24 @@ void DirEntryCpy(DirEntry* dest, const DirEntry* orig) {
     dest->name = dest->path + (orig->name - orig->path);
 }
 
+int compDirEntry(const void* e1, const void* e2) {
+    const DirEntry* entry1 = *((const DirEntry**) e1);
+    const DirEntry* entry2 = *((const DirEntry**) e2);
+    if (entry1->type == T_DOTDOT) return -1;
+    if (entry2->type == T_DOTDOT) return 1;
+    if (entry1->type != entry2->type)
+        return entry1->type - entry2->type;
+    return strncasecmp(entry1->name, entry2->name, 256);
+}
+
 void SortDirStruct(DirStruct* contents) {
-    for (u32 s = 0; s < contents->n_entries; s++) {
-        DirEntry* cmp0 = &(contents->entry[s]);
-        DirEntry* min0 = cmp0;
-        if (cmp0->type == T_DOTDOT) continue;
-        for (u32 c = s + 1; c < contents->n_entries; c++) {
-            DirEntry* cmp1 = &(contents->entry[c]);
-            if (min0->type != cmp1->type) {
-                if (min0->type > cmp1->type)
-                    min0 = cmp1;
-                continue;
-            }
-            if (strncasecmp(min0->name, cmp1->name, 256) > 0)
-                min0 = cmp1;
-        }
-        if (min0 != cmp0) {
-            DirEntry swap; // swap entries and fix names
-            DirEntryCpy(&swap, cmp0);
-            DirEntryCpy(cmp0, min0);
-            DirEntryCpy(min0, &swap);
-        }
-    }
+    DirEntry* entry_ptr[MAX_DIR_ENTRIES];
+    for (int i = 0; i < (int)contents->n_entries; i++)
+        entry_ptr[i] = &(contents->entry[i]);
+    qsort(entry_ptr, contents->n_entries, sizeof(DirEntry*), compDirEntry);
+    DirEntry tmp[MAX_DIR_ENTRIES];
+    for (int i = 0; i < (int)contents->n_entries; i++)
+        DirEntryCpy(&tmp[i], entry_ptr[i]);
+    for (int i = 0; i < (int)contents->n_entries; i++)
+        DirEntryCpy(&(contents->entry[i]), &tmp[i]);
 }
