372711ff5189f060a62fc44385daa3d5a3ae6941
QCFium <rin20042004@gmail.com>
2018-09-17 19:53:38 +0900

    Reduce memory usage

--- arm9/source/filesys/fsdir.c
+++ arm9/source/filesys/fsdir.c
@@ -6,23 +6,24 @@ void DirEntryCpy(DirEntry* dest, const DirEntry* orig) {
 }
 
 int compDirEntry(const void* e1, const void* e2) {
-    const DirEntry* entry1 = *((const DirEntry**) e1);
-    const DirEntry* entry2 = *((const DirEntry**) e2);
+    const DirEntry* entry1 = (const DirEntry*) e1;
+    const DirEntry* entry2 = (const DirEntry*) e2;
     if (entry1->type == T_DOTDOT) return -1;
     if (entry2->type == T_DOTDOT) return 1;
     if (entry1->type != entry2->type)
         return entry1->type - entry2->type;
-    return strncasecmp(entry1->name, entry2->name, 256);
+    return strncasecmp(entry1->path, entry2->path, 256);
 }
 
 void SortDirStruct(DirStruct* contents) {
-    DirEntry* entry_ptr[MAX_DIR_ENTRIES];
-    for (int i = 0; i < (int)contents->n_entries; i++)
-        entry_ptr[i] = &(contents->entry[i]);
-    qsort(entry_ptr, contents->n_entries, sizeof(DirEntry*), compDirEntry);
-    DirEntry tmp[MAX_DIR_ENTRIES];
-    for (int i = 0; i < (int)contents->n_entries; i++)
-        DirEntryCpy(&tmp[i], entry_ptr[i]);
-    for (int i = 0; i < (int)contents->n_entries; i++)
-        DirEntryCpy(&(contents->entry[i]), &tmp[i]);
+    qsort(contents->entry, contents->n_entries, sizeof(DirEntry), compDirEntry);
+    for (int i = 0; i < (int)contents->n_entries; i++) {
+        DirEntry* entry = &(contents->entry[i]);
+        if (entry->type == T_DOTDOT) {
+            entry->name = entry->path + 8;
+        } else {
+            char* slash = strrchr(entry->path, '/');
+            entry->name = slash ? slash + 1 : entry->path;
+        }
+    }
 }
