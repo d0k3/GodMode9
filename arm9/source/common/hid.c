1c08c2baace67cdfcefafcbf3ce2bd9cac4ed024
d0k3 <tore.anon@gmail.com>
2019-05-03 01:37:41 +0200

    Move touchbox functions to hid.c / hid.h

--- arm9/source/common/hid.c
+++ arm9/source/common/hid.c
@@ -106,6 +106,23 @@ bool HID_SetCalibrationData(const HID_CalibrationData *calibs, int point_cnt, in
     return true;
 }
 
+bool TouchBoxGet(u16* x, u16* y, u32* id, const TouchBox* tbs, const u32 tbn) {
+    *id = 0;
+
+    // read coordinates, check if inside touchbox
+    if (!HID_ReadTouchState(x, y)) return false;
+    for (u32 i = 0; i < tbn; i++) {
+        const TouchBox* tb = tbs + i;
+        if ((*x >= tb->x) && (*y >= tb->y) &&
+            (*x < tb->x + tb->w) && (*y < tb->y + tb->h)) {
+            *id = tb->id;
+            break;
+        }
+    }
+
+    return true;
+}
+
 u32 InputWait(u32 timeout_sec) {
     static u64 delay = 0;
     u64 timer = timer_start();
