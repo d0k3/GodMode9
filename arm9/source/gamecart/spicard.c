28ba6971a6329b46d1a7b4a2add403fbac86ad26
Balint Kovacs <dratini0@gmail.com>
2019-07-08 15:27:44 +0200

    First viable(-ish) prototype
    For some reason (messed up memory access?) the first two time I read the
    status register, I get garbage.
    
    Also:
    * Reinserting card breaks SPI (everything reads 0xff
    * No support for CTR carts for now

---
+++ arm9/source/gamecart/spicard.c
@@ -0,0 +1,139 @@
+/*
+ *   This file is part of fastboot 3DS
+ *   Copyright (C) 2019 derrek, profi200
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "types.h"
+#include "spicard.h"
+#include "delay.h"
+
+#define SPICARD_REGS_BASE  0x1000D800
+#define REG_NSPI_CNT       *((vu32*)(SPICARD_REGS_BASE + 0x00))
+#define REG_NSPI_DONE      *((vu32*)(SPICARD_REGS_BASE + 0x04))
+#define REG_NSPI_BLKLEN    *((vu32*)(SPICARD_REGS_BASE + 0x08))
+#define REG_NSPI_FIFO      *((vu32*)(SPICARD_REGS_BASE + 0x0C))
+#define REG_NSPI_STATUS    *((vu32*)(SPICARD_REGS_BASE + 0x10))
+#define REG_NSPI_AUTOPOLL  *((vu32*)(SPICARD_REGS_BASE + 0x14))
+#define REG_NSPI_INT_MASK  *((vu32*)(SPICARD_REGS_BASE + 0x18))
+#define REG_NSPI_INT_STAT  *((vu32*)(SPICARD_REGS_BASE + 0x1C))
+
+
+
+static inline void nspiWaitBusy(void)
+{
+	while(REG_NSPI_CNT & NSPI_CNT_ENABLE);
+}
+
+static inline void nspiWaitFifoBusy(void)
+{
+	while(REG_NSPI_STATUS & NSPI_STATUS_BUSY);
+}
+
+void SPICARD_init(void)
+{
+	static bool inited = false;
+	if(inited) return;
+	inited = true;
+
+	// TODO
+#define REG_CFG9_CARDCTL      *((vu16*)0x1000000C)
+#define REG_CFG9_CARDSTATUS   *((vu8* )0x10000010)
+#define REG_CFG9_CARDCYCLES0  *((vu16*)0x10000012)
+#define REG_CFG9_CARDCYCLES1  *((vu16*)0x10000014)
+
+#define REG_NTRCARDMCNT       *((vu16*)0x10164000)
+#define REG_NTRCARDROMCNT     *((vu32*)0x10164004)
+
+	REG_CFG9_CARDCYCLES0 = 0x1988;
+	REG_CFG9_CARDCYCLES1 = 0x264C;
+	// boot9 waits here. Unnecessary?
+
+	REG_CFG9_CARDSTATUS = 3u<<2;     // Request power off
+	while(REG_CFG9_CARDSTATUS != 0); // Aotomatically changes to 0 (off)
+	ioDelay(1000);
+
+	REG_CFG9_CARDSTATUS = 1u<<2;     // Prepare power on
+	ioDelay(10000);
+
+	REG_CFG9_CARDSTATUS = 2u<<2;     // Power on
+	ioDelay(27000);
+
+	// Switch to NTRCARD controller.
+	REG_CFG9_CARDCTL = 0;
+	REG_NTRCARDMCNT = 0xC000u;
+	REG_NTRCARDROMCNT = 0x20000000;
+	ioDelay(120000);
+
+	REG_CFG9_CARDCTL |= 1u<<8;
+
+	REG_NSPI_INT_MASK = NSPI_INT_TRANSF_END; // Disable interrupt 1
+	REG_NSPI_INT_STAT = NSPI_INT_AP_TIMEOUT | NSPI_INT_AP_SUCCESS | NSPI_INT_TRANSF_END; // Aknowledge
+}
+
+/*
+bool _SPICARD_autoPollBit(u32 params)
+{
+	REG_NSPI_AUTOPOLL = NSPI_AUTOPOLL_START | params;
+
+	u32 res;
+	do
+	{
+		__wfi();
+		res = REG_NSPI_INT_STAT;
+	} while(!(res & (NSPI_INT_AP_TIMEOUT | NSPI_INT_AP_SUCCESS)));
+	REG_NSPI_INT_STAT = res; // Aknowledge
+
+	return (res & NSPI_INT_AP_TIMEOUT) == 0; // Timeout error
+}
+*/
+
+void SPICARD_writeRead(NspiClk clk, const u32 *in, u32 *out, u32 inSize, u32 outSize, bool done)
+{
+	const u32 cntParams = NSPI_CNT_ENABLE | NSPI_CNT_BUS_1BIT | clk;
+
+	if(in)
+	{
+		REG_NSPI_BLKLEN = inSize;
+		REG_NSPI_CNT = cntParams | NSPI_CNT_DIRE_WRITE;
+
+		u32 counter = 0;
+		do
+		{
+			if((counter & 31) == 0) nspiWaitFifoBusy();
+			REG_NSPI_FIFO = *in++;
+			counter += 4;
+		} while(counter < inSize);
+
+		nspiWaitBusy();
+	}
+	if(out)
+	{
+		REG_NSPI_BLKLEN = outSize;
+		REG_NSPI_CNT = cntParams | NSPI_CNT_DIRE_READ;
+
+		u32 counter = 0;
+		do
+		{
+			if((counter & 31) == 0) nspiWaitFifoBusy();
+			*out++ = REG_NSPI_FIFO;
+			counter += 4;
+		} while(counter < outSize);
+
+		nspiWaitBusy();
+	}
+
+	if(done) REG_NSPI_DONE = NSPI_DONE;
+}
---
+++ arm9/source/gamecart/spicard.h
@@ -0,0 +1,96 @@
+#pragma once
+
+/*
+ *   This file is part of fastboot 3DS
+ *   Copyright (C) 2019 derrek, profi200
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "types.h"
+
+
+// REG_NSPI_CNT
+#define NSPI_CNT_BUS_1BIT    (0u)
+#define NSPI_CNT_BUS_4BIT    (1u<<12)
+#define NSPI_CNT_DIRE_READ   (0u)
+#define NSPI_CNT_DIRE_WRITE  (1u<<13)
+#define NSPI_CNT_ENABLE      (1u<<15)
+
+// REG_NSPI_DONE
+#define NSPI_DONE            (0u)
+
+// REG_NSPI_STATUS
+#define NSPI_STATUS_BUSY     (1u)
+
+// REG_NSPI_AUTOPOLL
+#define NSPI_AUTOPOLL_START  (1u<<31)
+
+// REG_NSPI_INT_MASK Bit set = disabled.
+// REG_NSPI_INT_STAT Status and aknowledge.
+#define NSPI_INT_TRANSF_END  (1u)    // Fires on (each?) auto poll try aswell
+#define NSPI_INT_AP_SUCCESS  (1u<<1) // Auto poll
+#define NSPI_INT_AP_TIMEOUT  (1u<<2) // Auto poll
+
+
+typedef enum
+{
+	NSPI_CLK_512KHz = 0u,
+	NSPI_CLK_1MHz   = 1u,
+	NSPI_CLK_2MHz   = 2u,
+	NSPI_CLK_4MHz   = 3u,
+	NSPI_CLK_8MHz   = 4u,
+	NSPI_CLK_16MHz  = 5u
+} NspiClk;
+
+
+
+/**
+ * @brief      Initializes the SPI buses. Call this only once.
+ */
+void SPICARD_init(void);
+
+/**
+ * @brief      Automatically polls a bit of the command response. Use with the macro below.
+ *
+ * @param[in]  params  The parameters. Use the macro below.
+ *
+ * @return     Returns false on failure/timeout and true on success.
+ */
+bool _SPICARD_autoPollBit(u32 params);
+
+/**
+ * @brief      Writes and/or reads data to/from a SPI device.
+ *
+ * @param[in]  clk      The clock frequency to use.
+ * @param[in]  in       Input data pointer for write.
+ * @param      out      Output data pointer for read.
+ * @param[in]  inSize   Input size. Must be <= 0x1FFFFF.
+ * @param[in]  outSize  Output size. Must be <= 0x1FFFFF.
+ * @param[in]  done     Set to true if this is the last transfer (chip select).
+ */
+void SPICARD_writeRead(NspiClk clk, const u32 *in, u32 *out, u32 inSize, u32 outSize, bool done);
+
+
+/**
+ * @brief      Automatically polls a bit of the command response.
+ *
+ * @param[in]  cmd      The command.
+ * @param[in]  timeout  The timeout. Must be 0-15. Tries = 31<<NspiClk + timeout.
+ * @param[in]  off      The bit offset. Must be 0-7.
+ * @param[in]  bitSet   Poll for a set ur unset bit.
+ *
+ * @return     Returns false on failure/timeout and true on success.
+ */
+#define SPICARD_autoPollBit(cmd, timeout, off, bitSet) _SPICARD_autoPollBit((bitSet)<<30 | (off)<<24 | (timeout)<<16 | (cmd))
