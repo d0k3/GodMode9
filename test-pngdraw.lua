bc3a21ba9dec8c33a76389737883dcb7f65c972a
ihaveahax <ian@ianburgwin.net>
2024-11-19 15:10:00 -0600

    remove test lua scripts in root

--- test-pngdraw.lua
+++
@@ -1,3 +0,0 @@
-UI.ShowPNG(1, "0:/art.png")
-UI.DrawPNG(1, 2, 2, "0:/art.png")
-UI.ShowPrompt("Press A")
--- test-showprompts.lua
+++
@@ -1,4 +0,0 @@
-UI.AskPrompt(UI.ShowKeyboardOrPrompt("Hi", 16, "Enter text:"))
-UI.AskPrompt(UI.ShowHexPrompt(0xF, 16, "Enter hex:"))
-UI.AskPrompt(UI.ShowNumberPrompt(53, "Enter number:"))
-UI.ShowPrompt("Press A")
