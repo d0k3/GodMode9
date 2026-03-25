ee0142baadd6989ff516138d09caad5e5bde3ff4
MisterSheeple <27652712+MisterSheeple@users.noreply.github.com>
2026-03-25 00:20:57 +0000

    Update lcode.c

--- arm9/source/lua/lcode.c
+++ arm9/source/lua/lcode.c
@@ -55,7 +55,7 @@ l_noret luaK_semerror (LexState *ls, const char *msg) {
 ** and returns 1. Otherwise, returns 0.
 */
 static int tonumeral (const expdesc *e, TValue *v) {
-  if (e->k == VJMP || hasjumps(e))
+  if (hasjumps(e))
     return 0;  /* not a numeral */
   switch (e->k) {
     case VKINT:
@@ -986,7 +986,7 @@ void luaK_exp2anyregup (FuncState *fs, expdesc *e) {
 ** or it is a constant.
 */
 void luaK_exp2val (FuncState *fs, expdesc *e) {
-  if (hasjumps(e))
+  if (e->k == VJMP || hasjumps(e))
     luaK_exp2anyreg(fs, e);
   else
     luaK_dischargevars(fs, e);
