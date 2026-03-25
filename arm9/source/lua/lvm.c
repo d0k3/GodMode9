991f6085a1eca1b55f831b9c826bd1010d5c21d8
MisterSheeple <27652712+MisterSheeple@users.noreply.github.com>
2026-03-25 00:28:12 +0000

    Update lvm.c

--- arm9/source/lua/lvm.c
+++ arm9/source/lua/lvm.c
@@ -339,7 +339,7 @@ void luaV_finishset (lua_State *L, const TValue *t, TValue *key,
       lua_assert(isempty(slot));  /* slot must be empty */
       tm = fasttm(L, h->metatable, TM_NEWINDEX);  /* get metamethod */
       if (tm == NULL) {  /* no metamethod? */
-		sethvalue2s(L, L->top.p, h);  /* anchor 't' */
+        sethvalue2s(L, L->top.p, h);  /* anchor 't' */
         L->top.p++;  /* assume EXTRA_STACK */
         luaH_finishset(L, h, key, slot, val);  /* set new value */
 		L->top.p--;
