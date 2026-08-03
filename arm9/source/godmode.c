c03c6dfde88724df88dac5f19c9456074856a552
git-effl <serenatamagnini@gmail.com>
2026-08-03 22:07:00 +0200

    Update godmode.c

--- arm9/source/godmode.c
+++ arm9/source/godmode.c
@@ -151,7 +151,7 @@ u32 SplashInit(const char* modestr) {
 
     DrawStringF(BOT_SCREEN, pos_xb, pos_yb, COLOR_STD_FONT, COLOR_STD_BG, "%s\n%*.*s\n%s\n \n \n%s\n%s\n \n%s\n%s",
         namestr, strlen(namestr), strlen(namestr),
-        "--------------------------------", "https://github.com/d0k3/GodMode9",
+        "--------------------------------", "https://github.com/d0k3/GodMode9";
         "Releases:", "https://github.com/d0k3/GodMode9/releases/", // this won't fit with a 8px width font
         "Hourlies:", "https://d0k3.secretalgorithm.com/");
     DrawStringF(BOT_SCREEN, pos_xu, pos_yu, COLOR_STD_FONT, COLOR_STD_BG, "%s", loadstr);
